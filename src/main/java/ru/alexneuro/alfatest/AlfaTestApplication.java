b1f1006326d97aa701fe5726a036a6a9eabb9368
alexneuro <alexneuro31@ya.ru>
2020-04-23 02:33:18 +0300

    Fix problem when args is null

--- src/main/java/ru/alexneuro/alfatest/AlfaTestApplication.java
+++ src/main/java/ru/alexneuro/alfatest/AlfaTestApplication.java
@@ -15,13 +15,18 @@ public class AlfaTestApplication implements ApplicationRunner {
     @Autowired
     private XMLParser xmlParser;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        if (args.length == 0)
+            throw new Exception("Path to XML file must be not empty! Please set Program arguments as file path");
+
         SpringApplication.run(AlfaTestApplication.class, args);
     }
 
     @Override
     public void run(ApplicationArguments args) {
         String fileName = args.getSourceArgs()[0];
-        xmlParser.parse(new File(fileName));
+        File xmlFile = new File(fileName);
+
+        xmlParser.parse(xmlFile);
     }
 }
